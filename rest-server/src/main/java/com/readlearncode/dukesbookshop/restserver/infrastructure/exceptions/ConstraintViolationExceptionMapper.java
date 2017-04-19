9f703b90bc851a432878cb11d1545c9f1b4a23fa
Alex Theedom <readlearncode@gmail.com>
2017-04-19 13:49:56 -0700

    End state for bean validation exception mapper

---
+++ rest-server/src/main/java/com/readlearncode/dukesbookshop/restserver/infrastructure/exceptions/ConstraintViolationExceptionMapper.java
@@ -0,0 +1,31 @@
+package com.readlearncode.dukesbookshop.restserver.infrastructure.exceptions;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.stream.Collectors;
+
+/**
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+
+        final String message = exception.getConstraintViolations().stream()
+                .map(cv -> extractPropertyName(cv.getPropertyPath().toString())
+                + " : " + cv.getMessage())
+                .collect(Collectors.joining(", "));
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .header("X-Validation-Failure", message)
+                .build();
+    }
+
+    private String extractPropertyName(String path){
+        return path.substring(path.lastIndexOf(".") + 1);
+    }
+}
